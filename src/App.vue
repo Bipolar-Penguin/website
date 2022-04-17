14ac186f9278583f2c81e69d0a86f8e36aca4727
d_mikhailets <d_mikhailets@wargaming.net>
2022-04-17 12:21:50 +0300

    add push

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,6 @@
+
+const app = initializeApp(firebaseConfig);
+
 <template>
   <div id="app" class="main_wrap">
     <TopMenu />
@@ -7,6 +10,19 @@
 <script>
 import TopMenu from "@/components/TopMenu.vue";
 import SessionPage from "@/components/SessionPage.vue";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCR0D9v-i7Eysaj2v7MF0LlfD3gfZFn0v8",
+  authDomain: "pwa-push-2118.firebaseapp.com",
+  projectId: "pwa-push-2118",
+  storageBucket: "pwa-push-2118.appspot.com",
+  messagingSenderId: "577307638683",
+  appId: "1:577307638683:web:223bb814fe1c3312d37f3e"
+};
+
+const app = initializeApp(firebaseConfig);
+
 
 export default {
   components: {
--- src/main.js
+++ src/main.js
@@ -3,8 +3,28 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import * as firebase from 'firebase/app';
+import 'firebase/messaging'
+import firebaseMessaging from './firebase'
+
+
+// // Initialize Firebase
+firebase.initializeApp({
+  'messagingSenderId': '744798928283'
+});
+
+navigator.serviceWorker.register('firebase-messaging-sw.js', {scope: "firebase-cloud-messaging-push-scope"})
+  .then((registration) => {
+      const messaging = firebase.messaging();
+      messaging.useServiceWorker(registration);
+
+  }).catch(err => {
+      console.log(err)
+  })
 
 Vue.config.productionTip = false
+
+Vue.prototype.$messaging = firebaseMessaging
 
 new Vue({
   router,
